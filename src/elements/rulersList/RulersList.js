6b61955a5df98ee455f077bc3279e8335dbf625a
Paweł Borowiec <pawel.2020.borowiec@gmail.com>
2022-04-20 10:49:22 +0200

    Added all elements for lists

--- src/elements/rulersList/RulersList.js
+++ src/elements/rulersList/RulersList.js
@@ -1,40 +1,41 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect} from 'react';
 import './RulersList.css';
 import Ruler from '../ruler/Ruler';
 import {Link } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
-class RulersList extends Component {
-  state = {
-    data: []
-  }
+function RulersList(props){
+  const [data, setData] = useState([])
+  const navigate = useNavigate(); 
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://countries-and-rulers-app.herokuapp.com/rulers', {mode: "cors"})
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        this.setState( {data})
-      });
-  }
+    .then(response => response.json())
+    .then(content => {
+      setData(content);
+    });
+  });
 
 
 
-  render() {
     return (
       <div class="rulerList">
             <table>
-              <tr>
-                <th>No</th>
-                <th>Name</th>
-                <th>Dynasty name</th>
-                <th>Actions</th>
-              </tr> 
-           {this.state.data.map((ruler, index) =><Ruler info={ruler} index={index}/>)}
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Name</th>
+                  <th>Dynasty name</th>
+                  <th>Actions</th>
+                </tr> 
+              </thead>
+           <tbody>
+            {data.map((ruler, index) =><Ruler info={ruler} index={index}/>)}
+           </tbody>
            </table>
            <Link to={'rulers/newRuler'}><button class="addButton">Add new Ruler</button></Link>
       </div>
     );
   }
-}
 
 export default RulersList;
